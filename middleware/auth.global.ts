1accd4d12307d3a641c030f3f2e973def54e151b
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-05-03 06:17:08 +0430

    edit middlewares

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const token = useCookie('access_token');
 
-  if (token) {
+  if (token.value) {
+    const member = (await $fetch(
+      `/api/members/me?token=${token.value}`,
+    )) as Member;
     to.params = {
       token: token.value,
+      ...member,
     };
   }
 
